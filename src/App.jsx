bac53fbf76390976a94ef4d25372c2d457fa83c1
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-22 12:24:19 +0300

    src klasoru icindeki components klasoru icinde headers.jsx component olusturuldu, headers.jsx component icinde logo, title, hamburger-menu eklendi, header.jsx component app.jsx component icinde cagirildi, app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
+import Headers from './Components/Headers'
 
 export default function App() {
 
   return (
-    <>
-    </>
+    <Headers />
   )
 }
---
+++ src/Components/Headers.jsx
@@ -0,0 +1,12 @@
+import Logo from '../assets/img/logo.png'
+import HamburgerMenu from '../assets/img/hamburger-menu.png'
+
+export default function Headers() {
+  return (
+    <header className="header">
+      <img src={Logo} className='logo' alt="" />
+      <h4>Sağlıklı Menü</h4>
+      <img src={HamburgerMenu} className='hamburger-menu' alt="" />
+    </header>
+  )
+}
