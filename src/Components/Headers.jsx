2601bfd174d81836aae32eb28c1d27306a887334
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-23 06:51:03 +0300

    src klasoru icindeki components klasoru icindeki headers.jsx component icinde main alaninda hero section alani olusturuldu, app.css dosyasinda css duzenlemesi yapildi

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -1,14 +1,28 @@
 import Logo from '../assets/img/logo.png'
 import HamburgerMenu from '../assets/img/hamburger-menu.png'
+import FeaturedMenus from '../assets/img/featured-menus.png'
+import FeaturedExampleMenu from '../assets/img/featured--example-menu.png'
 
 export default function Headers() {
   return (
-    <header className="header">
-      <nav className="navbar">
-        <img src={Logo} className='logo' alt="" />
-        <h4>Sağlıklı Menü</h4>
-        <img src={HamburgerMenu} className='hamburger-menu' alt="" />
-      </nav>
-    </header>
+    <>
+      <header className="header">
+        <nav className="navbar">
+          <img src={Logo} className='logo' alt="" />
+          <h4>Sağlıklı Menü</h4>
+          <img src={HamburgerMenu} className='hamburger-menu' alt="" />
+        </nav>
+      </header>
+
+      <main>
+        <section className="hero">
+          <div className="title-and-featured-menu-button">
+            <h6 className="title">Öne Çıkan Menü</h6>
+            <img src={FeaturedMenus} className='featured-menus' alt="" />
+          </div>
+          <img src={FeaturedExampleMenu} className='featured-example-menu' alt="" />
+        </section>
+      </main>
+    </>
   )
 }
