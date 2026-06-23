ea22a8032e15c677b1ee5e5f810ee0d927f73e3b
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-23 05:17:19 +0300

    src klasoru icindeki components klasoru icinde headers.jsx component olusturuldu, headers.jsx component icinde header alaninda bulunan logo, title, hamburger-menu header alani icinde olusturulan navbar alanina alindi

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -4,9 +4,11 @@ import HamburgerMenu from '../assets/img/hamburger-menu.png'
 export default function Headers() {
   return (
     <header className="header">
-      <img src={Logo} className='logo' alt="" />
-      <h4>Sağlıklı Menü</h4>
-      <img src={HamburgerMenu} className='hamburger-menu' alt="" />
+      <nav className="navbar">
+        <img src={Logo} className='logo' alt="" />
+        <h4>Sağlıklı Menü</h4>
+        <img src={HamburgerMenu} className='hamburger-menu' alt="" />
+      </nav>
     </header>
   )
 }
