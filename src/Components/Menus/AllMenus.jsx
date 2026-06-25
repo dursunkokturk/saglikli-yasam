7da95bd9a380e6579cffbbec633667ed8cd3be32
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-25 10:51:04 +0300

    src klasoru icindeki components klasoru icindeki menus klasoru icinde AllMenus.jsx component'i icinde menu listeleri baslik olarak eklendi, app.css dosyasinda css duzenlemesi yapildi

--- src/Components/Menus/AllMenus.jsx
+++ src/Components/Menus/AllMenus.jsx
@@ -4,13 +4,20 @@ import IconDown from '../../assets/img/icon-down.png'
 export default function AllMenus() {
   return (
     <main>
-      <section className="all-menus">
+      <section className="all-menus-write-and-filter">
         <h5 className="title">Tüm Menüler</h5>
         <label htmlFor="">
           <img src={IconFilter} className='icon-filter' alt="" />
           Filtre
           <img src={IconDown} className='icon-down' alt="" />
         </label>
+      </section>
+      <section className="menu-lists">
+        <label htmlFor="">Hepsi</label>
+        <label htmlFor="">Kahvaltı</label>
+        <label htmlFor="">Öğle Yemeği</label>
+        <label htmlFor="">Atıştırmalık</label>
+        <label htmlFor="">Akşam Yemeği</label>
       </section>
     </main>
   )
