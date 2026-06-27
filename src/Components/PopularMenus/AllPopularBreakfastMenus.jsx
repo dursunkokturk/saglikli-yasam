11818e3b9c93d74dd1ca6f55382524dafb8fe625
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-27 21:14:42 +0300

    src klasoru icindeki components klasoru icindeki PopularMenus Component icindeki AllPopularBreakfastMenus.jsx component icinde genel duzenleme yapildi, app.css dosyasinda css duzenlemeleri yapildi

--- src/Components/PopularMenus/AllPopularBreakfastMenus.jsx
+++ src/Components/PopularMenus/AllPopularBreakfastMenus.jsx
@@ -1,6 +1,6 @@
 import PopularMenus from '../../assets/img/popular-menus.png'
 import IconDown from '../../assets/img/icon-down.png'
-import AllPopularBreakfastMenu1 from '../../assets/img/popular-menu-1.png'
+import PopularBreakfastMenu1 from '../../assets/img/popular-breakfast-menu-1.png'
 import IconLevel from '../../assets/img/icon-level.png'
 import IconCalory from '../../assets/img/icon-calory.png'
 import IconBread from '../../assets/img/icon-bread.png'
@@ -10,37 +10,39 @@ import Star from '../../assets/img/star.png'
 
 export default function AllPopularBreakfastMenus() {
 
-  const healthScore = 9
-  const totalDots = 10
+  const healthScore = 4.8
+  const totalDots = 5
 
   return (
     <>
       <section className="all-popular-breakfast-menus">
-        <h5 className="title">Popüler Menüler</h5>
-        <img src={PopularMenus} className='popular-menus' alt="" />
-      </section>
+        <div className="write-and-photo">
+          <h5 className="title">Popüler Menüler</h5>
+          <img src={PopularMenus} className='popular-menus' alt="" />
+        </div>
 
-      <section className="all-popular-breakfast-card">
+        <div className="all-popular-breakfast-card">
 
-        <img src={AllPopularBreakfastMenu1} className='all-popular-breakfast-menu-photo' alt="" />
+          <img src={PopularBreakfastMenu1} className='all-popular-breakfast-menu-photo' alt="" />
 
-        <div className='all-popular-breakfast-content'>
-          <h5 className='all-popular-breakfast-name'>Feta Peyniri ve Zeytinli Yunan Salatası</h5>
-          <button className='all-popular-breakfast-add-button'>+</button>
+          <div className='all-popular-breakfast-content'>
+            <h5 className='all-popular-breakfast-name'>Yaban Mersini Proteinli Smoothie</h5>
+            <button className='all-popular-breakfast-add-button'>+</button>
 
-          <div className="meal-type">
-            <h6>Kahvaltı</h6>
-          </div>
-
-          <div className="health-score--score">
-            <div className="health-score">
-              <div className="star-number">
-                <img src={Star} className='star' alt="" />
-              </div>
+            <div className="meal-type">
+              <h6>Kahvaltı</h6>
             </div>
 
-            <div className="score">
-              <h6>{healthScore}/10</h6>
+            <div className="health-score--score">
+              <div className="health-score">
+                <div className="star-number">
+                  <img src={Star} className='star' alt="" />
+                </div>
+              </div>
+
+              <div className="score">
+                <h6>{healthScore}/5</h6>
+              </div>
             </div>
           </div>
         </div>
