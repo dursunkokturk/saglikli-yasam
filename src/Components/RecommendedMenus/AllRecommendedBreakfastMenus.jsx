d0e2f0f7450e77b56c1e386bfedf7f58f37c7ba2
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-30 19:43:58 +0300

    src klasoru icindeki components klasoru icindeki RecommendedMenus Component icinde AllRecommendedBreakfastMenus.jsx component icinde yeni kahvalti menusu eklendi

--- src/Components/RecommendedMenus/AllRecommendedBreakfastMenus.jsx
+++ src/Components/RecommendedMenus/AllRecommendedBreakfastMenus.jsx
@@ -1,7 +1,7 @@
 import PopularMenus from '../../assets/img/popular-menus/popular-menus.png'
 import IconDown from '../../assets/img/icons/icon-down.png'
 import RecommendedBreakfastMenu1 from '../../assets/img/recommended-menus/recommended-breakfast-menu-1.png'
-import PopularBreakfastMenu2 from '../../assets/img/popular-menus/popular-breakfast-menu-2.png'
+import RecommendedBreakfastMenu2 from '../../assets/img/recommended-menus/recommended-breakfast-menu-2.png'
 import IconLevel from '../../assets/img/icons/icon-level.png'
 import IconCalory from '../../assets/img/icons/icon-calory.png'
 import IconBread from '../../assets/img/icons/icon-bread.png'
@@ -25,6 +25,49 @@ export default function AllRecommendedBreakfastMenus() {
 
             <div className="all-recommended-breakfast-name--all-recommended-breakfast-add-button--meal-type">
               <h5 className='all-recommended-breakfast-name'>Badem Ezmesi ve Meyveli Yulaf Lapası</h5>
+              <div className="all-recommended-breakfast-add-button--meal-type">
+                <button className='all-recommended-breakfast-add-button'>+</button>
+
+                <div className="meal-type">
+                  <h6>Kahvaltı</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='all-recommended-breakfast-content'>
+            <div className="health-score--score">
+              <div className="nutritional-information-specific-to-the-menu">
+                <div className="calory-information">
+                  <img src={IconCalory} className='icon-calory' alt="" />
+                  <h6>320 kcal</h6>
+                </div>
+                <div className="vitamin-information">
+                  <img src={IconBread} alt="" />
+                  <h6>30 gr C</h6>
+                </div>
+                <div className="protein-information">
+                  <img src={IconFish} alt="" />
+                  <h6>14 gr P</h6>
+                </div>
+                <div className="fats-information">
+                  <img src={IconDrop} alt="" />
+                  <h6>18 gr F</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <section className="all-recommended-breakfast-menus">
+        <div className="all-recommended-breakfast-card">
+
+          <div className="all-recommended-breakfast-menu-photo--all-recommended-breakfast-name--all-recommended-breakfast-add-button--meal-type--all-recommended-breakfast-add-button--meal-type">
+            <img src={RecommendedBreakfastMenu2} className='all-recommended-breakfast-menu-photo' alt="" />
+
+            <div className="all-recommended-breakfast-name--all-recommended-breakfast-add-button--meal-type">
+              <h5 className='all-recommended-breakfast-name'>Ispanaklı Çırpılmış Yumurta ve Tam Tahıllı Tost</h5>
               <div className="all-recommended-breakfast-add-button--meal-type">
                 <button className='all-recommended-breakfast-add-button'>+</button>
 
