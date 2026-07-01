0b30375f51510c0b8b6dfcda6f9818dd9cf421b3
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-07-01 07:54:36 +0300

    src klasoru icindeki components klasoru icindeki Menus Component icinde FeaturedMenu.jsx component icinde css isimleri guncellendi, app.css dosyasinda css isimlerinin guncel hali uygulandi

--- src/Components/Menus/FeaturedMenu.jsx
+++ src/Components/Menus/FeaturedMenu.jsx
@@ -9,53 +9,53 @@ export default function FeaturedMenu() {
     <main>
 
       <section className="hero">
-        <div className="title-and-featured-menu-button">
+        <div className="header">
           <h6 className="title">Öne Çıkan Menü</h6>
-          <img src={FeaturedMenus} className='featured-menus' alt="" />
+          <img src={FeaturedMenus} className='menuIcon' alt="" />
         </div>
-        <img src={FeaturedExampleMenu} className='featured-example-menu' alt="" />
+        <img src={FeaturedExampleMenu} className='cover' alt="" />
       </section>
 
       <section className="features">
         <h3>Izgara Hindi Göğsü, Buharda Pişirilmiş Kuşkonmaz ve Esmer Pirinç</h3>
         <div className="meals">
-          <div className="meal-name-and-evaluation-score">
-            <div className="meal-name">
+          <div className="meta">
+            <div className="badge">
               Öğlen
             </div>
-            <div className="evaluation-score">
+            <div className="rating">
               <img src={Star} alt="" />
               <h6>4.8/5 (125 inceleme)</h6>
             </div>
           </div>
-          <div className="recipe-summary">
+          <div className="details">
             <div className="left">
-              <div className="recipe-detail-card">
-                <img src={IconLevel} className='icon-level' alt="" />
-                <div className="recipe-detail-text">
+              <div className="detailCard">
+                <img src={IconLevel} className='icon' alt="" />
+                <div className="detailText">
                   <h6 className='level'>Zorluk</h6>
                   <h6 className='name'>Orta</h6>
                 </div>
               </div>
-              <div className="recipe-detail-card">
-                <img src={IconLevel} className='icon-level' alt="" />
-                <div className="recipe-detail-text">
+              <div className="detailCard">
+                <img src={IconLevel} className='icon' alt="" />
+                <div className="detailText">
                   <h6 className='level'>Sağlık Puanı</h6>
                   <h6 className='name'>85/100</h6>
                 </div>
               </div>
             </div>
             <div className="right">
-              <div className="recipe-detail-card">
-                <img src={IconLevel} className='icon-level' alt="" />
-                <div className="recipe-detail-text">
+              <div className="detailCard">
+                <img src={IconLevel} className='icon' alt="" />
+                <div className="detailText">
                   <h6 className='level'>Pişirme Süresi</h6>
                   <h6 className='name'>10 Dakika</h6>
                 </div>
               </div>
-              <div className="recipe-detail-card">
-                <img src={IconLevel} className='icon-level' alt="" />
-                <div className="recipe-detail-text">
+              <div className="detailCard">
+                <img src={IconLevel} className='icon' alt="" />
+                <div className="detailText">
                   <h6 className='level'>Toplam Adım Sayısı</h6>
                   <h6 className='name'>4 Adım</h6>
                 </div>
@@ -66,34 +66,34 @@ export default function FeaturedMenu() {
         </div>
       </section>
 
-      <section className="nutritional-values">
+      <section className="nutrition">
         <div className="left">
-          <div className="nutritional-values-detail-card">
+          <div className="nutritionCard">
             <img src={IconCalory} className='icon-calory' alt="" />
-            <div className="nutritional-values-detail-text">
+            <div className="nutritionText">
               <h6 className='title'>Kalori</h6>
               <h6 className='value'>450 kcal</h6>
             </div>
           </div>
-          <div className="nutritional-values-detail-card">
+          <div className="nutritionCard">
             <img src={IconCalory} className='icon-calory' alt="" />
-            <div className="nutritional-values-detail-text">
+            <div className="nutritionText">
               <h6 className='title'>Protein</h6>
               <h6 className='value'>35 gr</h6>
             </div>
           </div>
         </div>
         <div className="right">
-          <div className="nutritional-values-detail-card">
+          <div className="nutritionCard">
             <img src={IconCalory} className='icon-calory' alt="" />
-            <div className="nutritional-values-detail-text">
+            <div className="nutritionText">
               <h6 className='title'>Karbonhidratlar</h6>
               <h6 className='value'>40 gr</h6>
             </div>
           </div>
-          <div className="nutritional-values-detail-card">
+          <div className="nutritionCard">
             <img src={IconCalory} className='icon-calory' alt="" />
-            <div className="nutritional-values-detail-text">
+            <div className="nutritionText">
               <h6 className='title'>Yağ</h6>
               <h6 className='value'>12 gr</h6>
             </div>
